049eff17d05d4aba5367e3c57a015b18f476428a
Avinash <np6848586@gmail.com>
2025-07-13 22:27:30 +0530

    changes

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Menu, X } from "lucide-react";
-import cv from "../assets/resume.pdf";
+
 import profilePic from "../assets/profile.jpg"; // make sure the image exists
 
 const Navbar = () => {
